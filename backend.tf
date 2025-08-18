1af42092f1156575b95620dd2c527ad507b7e650
zacharykwek15 <zhi_hong_sn@yahoo.com.sg>
2025-08-18 18:09:22 +0800

    Removed backend.tf

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "zhbucket123"  # Backend bucket (must already exist)
-    key    = "terraform.tfstate"            # File path within the bucket
-    region = "ap-southeast-1"
-  }
-}
