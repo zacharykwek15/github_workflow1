fb77295c6573b66555e99da4ec6d38e8d9c301db
zacharykwek15 <zhi_hong_sn@yahoo.com.sg>
2025-08-24 23:12:26 +0800

    .

--- main.tf
+++ main.tf
@@ -1,13 +1,22 @@
-provider "aws" {
-  region = "ap-southeast-1"
-}
-
 terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.95.0, < 6.0.0"
+    }
+  }
+
   backend "s3" {
     bucket = "sctp-ce10-tfstate"
-    key    = "ce10grp2/dev/terraform.tfstate" #Change this
+    key    = "ce10grp2/dev/terraform.tfstate"
     region = "ap-southeast-1"
   }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
 }
 
 resource "aws_s3_bucket" "s3_tf" {
