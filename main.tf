384d1a8230e0aaf1cd67ed08c1174a90a2182764
zacharykwek15 <zhi_hong_sn@yahoo.com.sg>
2025-08-24 23:06:32 +0800

    Adding new s3 bucket

--- main.tf
+++ main.tf
@@ -1,12 +1,15 @@
 provider "aws" {
-  region = "ap-southeast-1"  # Singapore region
+  region = "ap-southeast-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "zhbucket123"
-
-  tags = {
-    Name        = "Demo S3 Bucket"
-    Environment = "Dev"
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce10-tfstate"
+    key    = "ce10grp2/dev/terraform.tfstate"  #Change this
+    region = "ap-southeast-1"
   }
+}
+
+resource "aws_s3_bucket" "s3_tf" {
+  bucket_prefix =  "zh_s3_bucket"
 }
